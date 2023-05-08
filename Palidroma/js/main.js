61772fde700c883340dbe9112acbd3e2e6e65de0
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-05-08 16:19:40 +0200

    reverse della parola

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -1,20 +1,23 @@
 //chiediamo all'utente di inserire un parola o una frase
 let input = prompt("inserisci parola o frase");
-console.log(input);
+console.log("messaggio utente: " + input);
 
 //creiamo una funzione per verificare la palindromità
-function verificaPali() {
+function verificaPali(parola) {
 
     //eliminiamo eventuali spazi o punteggiatura
-    let parolaClean = input.replace(/[^\w]+/g, "");
-    console.log(parolaClean);
+    let parolaClean = parola.replace(/[^\w]+/g, "");
+    console.log("parola unita: " + parolaClean);
 
     //reversiamo la parola
+    let parolaReverse = parolaClean.split("").reverse().join("");
+    console.log("parola reversata: " + parolaReverse);
 
     //confrontiamola con la vecchia parola
+    
 
     //stampiamo il risultato
 
 };
 
-verificaPali();
+verificaPali(input);
