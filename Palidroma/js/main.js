55c11ae9b0c5fd236ddd908fa6da0ecba4e5c52e
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-05-08 16:04:06 +0200

    eliminazione spazi e punteggiatura

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -1,8 +1,20 @@
 //chiediamo all'utente di inserire un parola o una frase
+let input = prompt("inserisci parola o frase");
+console.log(input);
 
+//creiamo una funzione per verificare la palindromità
+function verificaPali() {
 
-//eliminiamo eventuali spazi o virgole
+    //eliminiamo eventuali spazi o punteggiatura
+    let parolaClean = input.replace(/[^\w]+/g, "");
+    console.log(parolaClean);
 
-//reversiamo la parola
+    //reversiamo la parola
 
-//confrontiamola con la vecchia parola
+    //confrontiamola con la vecchia parola
+
+    //stampiamo il risultato
+
+};
+
+verificaPali();
